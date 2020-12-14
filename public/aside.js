c481d721d6a9ace0134c491448d3996d337a3a55
Cheewbacca <vanya.timoschenko85@gmail.com>
2020-12-14 08:45:47 +0200

    in progress

--- public/aside.js
+++ public/aside.js
@@ -575,7 +575,7 @@ ready(function() {
         console.log("this is not FAQ page");
     }
 
-    const urlName = window.location;
+    const urlName = window.location.href;
 
     if (urlName.includes('guest-demo') || urlName.indexOf('guest-demo') !== 1){
         document.getElementsByClassName('login_block')[0].style.display = 'none';
