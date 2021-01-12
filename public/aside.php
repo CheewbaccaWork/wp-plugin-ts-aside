83e27d4de2a4fc3726db15f676469ee3de0da1a9
Cheewbacca <vanya.timoschenko85@gmail.com>
2021-01-12 19:43:19 +0200

    new site added

--- public/aside.php
+++ public/aside.php
@@ -467,6 +467,10 @@
       $GLOBALS['login'] = 'livemarkets-api';
       $GLOBALS['password'] = 'ef3d940f';
       $GLOBALS['url'] = "https://platform-api.tradesmarter.com/index/get-session?userID=" . $_COOKIE['userID'];;
+    } else if ($result_general[0]->api_host == 'myeasytrade.wpengine.com') {
+      $GLOBALS['login'] = 'easytrade-api';
+      $GLOBALS['password'] = 'a8194041';
+      $GLOBALS['url'] = "https://Platform-api.hk-a.tradesmarter.com/index/get-session?userID=" . $_COOKIE['userID'];;
     }
 
     // Please, add here new statement for new site :
@@ -535,6 +539,10 @@ $accountLevel = "";
       $GLOBALS['login'] = 'livemarkets-api';
       $GLOBALS['password'] = 'ef3d940f';
       $GLOBALS['url2'] = "https://platform-api.tradesmarter.com/user/info?session=" . $GLOBALS['session'];
+    } else if ($result_general[0]->api_host == 'myeasytrade.wpengine.com') {
+      $GLOBALS['login'] = 'easytrade-api';
+      $GLOBALS['password'] = 'a8194041';
+      $GLOBALS['url2'] = "https://Platform-api.hk-a.tradesmarter.com/user/info?session=" . $GLOBALS['session'];
     } 
 
     // Please, add here new statement for new site :
